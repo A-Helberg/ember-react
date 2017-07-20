a8110e21ab855242cb082c6b8d411f8c33503466
Andre Helberg <helberg.andre@gmail.com>
2017-07-20 16:23:35 +0200

    render a react component

---
+++ app/components/hello.jsx
@@ -0,0 +1,11 @@
+import React from 'npm:react';
+
+export default class Hello extends React.Component {
+  render() {
+    return (
+    <div>
+      Hello React!
+    </div>
+    );
+  }
+}
--- app/resolver.js
+++ app/resolver.js
@@ -1,3 +1,32 @@
+import Ember from 'ember';
 import Resolver from 'ember-resolver';
 
-export default Resolver;
+import preactComponent from 'ember-react/components/react-component';
+
+const { get } = Ember;
+
+export default Resolver.extend({
+  resolveComponent(parsedName) {
+    console.log("resolve preact component");
+    const result = this.resolveOther(parsedName);
+
+    if (!result) {
+      return result;
+    }
+
+    if (get(result, 'isComponentFactory')) {
+      return result;
+    } else {
+      return preactComponent.extend({
+        reactComponent: result
+      });
+    }
+  },
+
+  resolveReactComponent(parsedName) {
+    parsedName.type = 'component';
+    const result = this.resolveOther(parsedName);
+    parsedName.type = 'preact-component';
+    return result;
+  }
+});
