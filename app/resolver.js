5867578d98ab27da09c6930dbc404d22889621ab
Andre Helberg <helberg.andre@gmail.com>
2017-07-21 09:46:26 +0200

    Add react files

--- app/resolver.js
+++ app/resolver.js
@@ -1,3 +1,16 @@
+import Ember from 'ember';
 import Resolver from 'ember-resolver';
 
-export default Resolver;
+// import preactComponent from 'ember-react/components/react-component';
+
+const { get } = Ember;
+
+export default Resolver.extend({
+  resolveReactComponent(parsedName) {
+    debugger
+    parsedName.type = 'component';
+    const result = this.resolveOther(parsedName);
+    parsedName.type = 'preact-component';
+    return result;
+  }
+});
