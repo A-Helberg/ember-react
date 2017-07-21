ca09ef20635e45683bd00dd03185cea3483a3b61
Andre Helberg <helberg.andre@gmail.com>
2017-07-21 09:42:47 +0200

    Clean up resolver

--- app/resolver.js
+++ app/resolver.js
@@ -1,28 +1,9 @@
 import Ember from 'ember';
 import Resolver from 'ember-resolver';
 
-import preactComponent from 'ember-react/components/react-component';
-
 const { get } = Ember;
 
 export default Resolver.extend({
-  resolveComponent(parsedName) {
-    console.log("resolve preact component");
-    const result = this.resolveOther(parsedName);
-
-    if (!result) {
-      return result;
-    }
-
-    if (get(result, 'isComponentFactory')) {
-      return result;
-    } else {
-      return preactComponent.extend({
-        reactComponent: result
-      });
-    }
-  },
-
   resolveReactComponent(parsedName) {
     parsedName.type = 'component';
     const result = this.resolveOther(parsedName);
